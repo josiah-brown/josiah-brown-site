aabb69d052b1373dd927ff1932cd41bc0254178c
Josiah Brown <josiahbrown321@gmail.com>
2022-10-13 10:06:30 -0500

    Add opacity fade in on page load

--- src/components/page_container/PageContainer.jsx
+++ src/components/page_container/PageContainer.jsx
@@ -4,7 +4,7 @@ const PageContainer = (props) => {
   return (
     <div
       data-testid="page-container"
-      className="relative adjusted_max_h bg-white dark:bg-neutral-800"
+      className="opacity_fade_in relative adjusted_max_h bg-white dark:bg-neutral-800"
     >
       {props.children}
     </div>
